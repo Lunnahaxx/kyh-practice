610895cfd44da84aedfa8e67646dd948fd05505a
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-01 10:59:46 +0200

    Number quess commit, sv + räknare,  + uppg 5.1(while-loop tiilagd),  + flyttat "antal_gissningar = antal_gissningar + 1" från båda fel "if as_number < n:" till under "if as_number == n:" för att få mindre kod.

--- guess_number.py
+++ guess_number.py
@@ -15,13 +15,12 @@ def mainloop():
         if as_number == n:
             print("Det var rätt du satte det på", antal_gissningar, "gissningar. ")
             break
+        antal_gissningar = antal_gissningar + 1
 
         if as_number < n:
             print("Tyvärr fel, mitt nummer är högre. Försök igen!")
-            antal_gissningar = antal_gissningar + 1
 
         if as_number > n:
             print("Tyvärr fel, mitt nummer är lägre. Försök igen!")
-            antal_gissningar = antal_gissningar + 1
 
 mainloop()
