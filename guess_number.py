0811976ea0bddc4f118d4a60589dc61b4e2df1fe
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-01 10:50:09 +0200

    Number quess commit, sv + räknare,  + uppg 5.1(while-loop tiilagd),

--- guess_number.py
+++ guess_number.py
@@ -1,22 +1,27 @@
-import random
+def mainloop():
 
-n = random.randint(1, 3)
-print("Jag tänker på ett nummer mellan 1 och 100. Gissa vilket.")
 
-antal_gissningar = 1
+    import random
 
-while True:
-    text = input("Din gissning: ")
-    as_number = int(text)
+    n = random.randint(1, 3)
+    print("Jag tänker på ett nummer mellan 1 och 100. Gissa vilket.")
 
-    if as_number == n:
-        print("Det var rätt du satte det på", antal_gissningar, "gissning. ")
-        break
+    antal_gissningar = 1
 
-    if as_number < n:
-        print("Tyvärr fel, mitt nummer är högre. Försök igen!")
-        antal_gissningar = antal_gissningar + 1
+    while True:
+        text = input("Din gissning: ")
+        as_number = int(text)
 
-    if as_number > n:
-        print("Tyvärr fel, mitt nummer är lägre. Försök igen!")
-        antal_gissningar = antal_gissningar + 1
+        if as_number == n:
+            print("Det var rätt du satte det på", antal_gissningar, "gissningar. ")
+            break
+
+        if as_number < n:
+            print("Tyvärr fel, mitt nummer är högre. Försök igen!")
+            antal_gissningar = antal_gissningar + 1
+
+        if as_number > n:
+            print("Tyvärr fel, mitt nummer är lägre. Försök igen!")
+            antal_gissningar = antal_gissningar + 1
+
+mainloop()
