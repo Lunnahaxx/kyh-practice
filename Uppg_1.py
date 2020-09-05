d46361a32ad84131938611638e37d522064bc017
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-05 23:42:47 +0200

    Uppg_1 - Task Assignment

---
+++ Uppg_1.py
@@ -0,0 +1,11 @@
+"""Task Assignment - Uppg_1
+Ex1: Fråga användaren "What is your name?" och lagra svaret
+i en variabel med namnet "name"
+
+name = input("What is your name?")
+
+Ex2: Slå ihop textsträngen "Your name is: " och svaret som
+lagrats i variabeln "name", och skriv ut resultatet på skärmen
+
+print("Your name is: " + name)"""
+
