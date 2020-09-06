5a4da33d10a740a15cd98d8b1444e556a47b71a5
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-06 11:17:02 +0200

    Added code, indented the code.

--- Uppg_3_gissa_tal.py
+++ Uppg_3_gissa_tal.py
@@ -20,3 +20,23 @@ print("Wrong, my number is higher... Try again!")
 
 if as_number > n:
 print("Wrong, my number is lower... Try again!")"""
+
+
+import random
+
+n = random.randint(1, 20)
+print("I'm thinking of a value between 1 and 30. Guess which?")
+
+while True:
+    text = input("Your guess: ")
+    as_number = int(text)
+
+    if as_number == n:
+        print("Correct!")
+        break
+
+    if as_number < n:
+        print("Wrong, my number is higher.. Try again!")
+
+    if as_number > n:
+        print("Wrong, my number is lower... Try again!")
