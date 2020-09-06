41f3a460e6d5fbe999dc4afa5211d8e7b86f9f29
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-06 12:57:08 +0200

    Task 1. Added mainloop. Had to move "antal_gissningar = 1" inside mainloop. Intended code.

--- Uppg_5_mer_ändringar_av_gissa.py
+++ Uppg_5_mer_ändringar_av_gissa.py
@@ -15,25 +15,27 @@ def ask_number():
     tal = int(text)
     return tal"""
 
-
 import random
 
 n = random.randint(1, 100)
 print("Jag tänker på ett nummer mellan 1 och 100. Du skall nu gissa vilket.")
 
-antal_gissningar = 1
+def mainloop():
 
-while True:
-    text = input("Vad är din gissning: ")
-    as_number = int(text)
+    antal_gissningar = 1
+    while True:
+        text = input("Vad är din gissning: ")
+        as_number = int(text)
 
-    if as_number == n:
-        print("Det var rätt du satte det på", antal_gissningar, "gissningar. ")
-        break
-    antal_gissningar = antal_gissningar + 1
+        if as_number == n:
+            print("Det var rätt du satte det på", antal_gissningar, "gissningar. ")
+            break
+        antal_gissningar = antal_gissningar + 1
 
-    if as_number < n:
-        print("Tyvärr fel, mitt nummer är högre. Försök igen!")
+        if as_number < n:
+            print("Tyvärr fel, mitt nummer är högre. Försök igen!")
 
-    if as_number > n:
-        print("Tyvärr fel, mitt nummer är lägre. Försök igen!")
+        if as_number > n:
+            print("Tyvärr fel, mitt nummer är lägre. Försök igen!")
+
+mainloop()
