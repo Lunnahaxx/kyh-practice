d73dc23d02326a29094c61488fa5d01fd6565828
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-07 15:15:57 +0200

    Add text and first category of print.

---
+++ Uppg_13_Mad_Magasin.py
@@ -0,0 +1,50 @@
+s = """Heavy metal-gruppen Hammerfall har tagit fram en tröja där alla intäkter
+ från försäljningen oavkortat ska gå till Mora IK. Detta skriver klubben på sin hemsida.
+
+Hammerfall har även vid tidigare tillfällen stöttat klubben från Dalarna.
+Bland annat har man gjort en låt åt klubben. Nu är det dags igen.
+Bandet har plockat fram en tröja, föreställande musikanternas maskot
+Hector iförd Mora IK-kläder.
+Tröja kommer finnas i 250 exemplar och kostar 249 kronor styck.
+Eftersom alla intäkter oavkortat går till klubben så 62 250 kronor om alla säljs.
+Sångaren Joacim Cans är uppvuxen i Mora.
+— Min förhoppning är också att vara på plats någon dag under kommande
+säsong för signering, säger han i en kommentar på klubben hemsida."""
+
+import random
+
+#kat1_klubben = ["koalan", "barnen"]
+#kat2_tröjan = ["mössan", "fingerborgen"]
+#kat3_Hammerfall = ["koalan", "barnen"]
+#kat4_Mora = ["Barcelona", "Bangkok"]
+
+#klubben = (f"{kat1_klubben[random.randint(0, 1)]}
+
+kat1_klubben = ["koalan", "barnen"]
+kat2_tröjan = ["mössan", "fingerborgen"]
+
+#print(kat1_klubben[])
+
+#print(f{ord_byte[radnom.randint(0, 1)]} har även vid tidigare tillfällen stöttat ordbyte_ f rån Dalarna.
+
+print(f"{kat1_klubben[random.randint(0, 1)]} har även vid "
+      f"tidigare tillfällen stöttat {kat2_tröjan[random.randint(0, 1)]} rån Dalarna.")
+
+
+#klubben = Barcelona, Bangkog,
+#tröjan = mössan, fingerborgen
+#Hammerfall = Koalan, barnen
+#Mora = Barcelona, Bangkok
+
+"""Heavy metal-gruppen Hammerfall har tagit fram en tröja där alla intäkter
+ från försäljningen oavkortat ska gå till Mora IK. Detta skriver klubben på sin hemsida.
+
+Hammerfall har även vid tidigare tillfällen stöttat klubben från Dalarna.
+Bland annat har man gjort en låt åt klubben. Nu är det dags igen.
+Bandet har plockat fram en tröja, föreställande musikanternas maskot
+Hector iförd Mora IK-kläder.
+Tröja kommer finnas i 250 exemplar och kostar 249 kronor styck.
+Eftersom alla intäkter oavkortat går till klubben så 62 250 kronor om alla säljs.
+Sångaren Joacim Cans är uppvuxen i Mora.
+— Min förhoppning är också att vara på plats någon dag under kommande
+säsong för signering, säger han i en kommentar på klubben hemsida."""
