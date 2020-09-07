ed7e90e945d8173d9b5a654c20cdee5b03924fef
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-07 10:53:00 +0200

    ändring input

--- Uppg_12_listor.py
+++ Uppg_12_listor.py
@@ -1,8 +1,10 @@
 def is_it_too_long(name):
     return len(name) > 5
 
+
 def main():
-    students = ["anna", "beatrice", "cecilia", "doris", "esmeralda", "frida", "gunilla"]
+    #students = ["anna", "beatrice", "cecilia", "doris", "esmeralda", "frida", "gunilla"]
+    students = input("Skriv studenternas namn med komma emellan: ").split(',')
     for name in students:
         if is_it_too_long(name):
             print(f"{name} är för långt!")
@@ -13,8 +15,8 @@ if __name__ == '__main__':
 
 """
 12.1 Indentera koden så att programmet fungerar hos er!
-12.2 Ändra så att användaren matar in studenternas namn separerade med ",".
-12.3 Ändra så att de namn skrivs ut med Stor Bokstav i början.
+12.2 Låt användaren skriva listan på studenter som en sträng, t.ex. "kalle,adam,eva" så att listan inte är hårdkodad i programmet
+12.3 Ändra så namn skrivs ut med Stor Bokstav i början.
 12.4 Ändra så att användaren matar in maxlängden på ett namn, och om det blir en ValueError exception så antas värdet "5" gälla
 
 Tips: Googla eller kör help(..) i Python Console på följande:
