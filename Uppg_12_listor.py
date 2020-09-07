2e2e505552f7db2f5b75f7c477d7cf98e17d809a
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-07 11:53:02 +0200

    12.4 add fixed_name

--- Uppg_12_listor.py
+++ Uppg_12_listor.py
@@ -1,7 +1,6 @@
 def is_it_too_long(name, max_length):
     return len(name) > max_length
 
-
 def main():
     #students = ["anna", "beatrice", "cecilia", "doris", "esmeralda", "frida", "gunilla"]
     try:
@@ -10,10 +9,9 @@ def main():
         max_length = 5
     students = input("Skriv studenternas namn med komma emellan: ").split(',')
     for name in students:
+        fixed_name = name.strip()
         if is_it_too_long(name, max_length):
-            print(f"{name.title()} är för långt!")
-
-
+            print(f"{fixed_name.title()} är för långt!")
 
 
 if __name__ == '__main__':
