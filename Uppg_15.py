ce64195131749f4305562da0ae1f68b89cba79af
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-08 11:33:25 +0200

    Uppd to actually Uppg. 15.

---
+++ Uppg_15.py
@@ -0,0 +1,14 @@
+"""Ett litet potpurri av småproblem att lösa tillsammans!
+
+15.1 Implementera "wordcount" som jag och Christoffer byggde
+15.2 Ta in en godtycklig text (testa att copy-paste från lorumipsum.com) och skriv ut hur många vokaler som finns i strängen. Tips: "a" in "abcd" är True!
+15.3 Göteborgsvarvet, vilken placering kom XYZ på? Implementera resten av detta program:
+    runners_in_order = “Lisa Lasse Louise Leopold Lova Love Lennart Lena Lisette Linus”.split()
+    vem = input(“Ange löpare du söker placering för”)"""
+
+15.1 Implementera "wordcount" som jag och Christoffer byggde
+15.2 Ta in en godtycklig text (testa att copy-paste från lorumipsum.com) och skriv ut hur många vokaler som finns i strängen. Tips: "a" in "abcd" är True!
+15.3 Göteborgsvarvet, vilken placering kom XYZ på? Implementera resten av detta program:
+    runners_in_order = “Lisa Lasse Louise Leopold Lova Love Lennart Lena Lisette Linus”.split()
+    vem = input(“Ange löpare du söker placering för”)
+
