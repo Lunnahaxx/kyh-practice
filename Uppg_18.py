72711a1f193759f8fea018756a230d2acec111c1
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-10 11:07:24 +0200

    18.2 och 18.3

--- Uppg_18.py
+++ Uppg_18.py
@@ -28,20 +28,24 @@ else:
 
 
 
-people = {
-'Fredrik': '0702778511',
-'Olof': '123456789',
-'Lisa': '9999999999',
-'Bodil': '555-666-789'
-}
-print(people)
-vem = input("Vem vill du ringa?")
 
-if vem not in people:
-    print("Sorry hörru, vet ej vem detta är. Har endast VIP i min katalog")
-else:
-  number = people[vem]
-  print(f"Numret till {vem} är {number}")
+#print(people)
+
+def main():
+    people = {
+        'Fredrik': '0702778511',
+        'Olof': '123456789',
+        'Lisa': '9999999999',
+        'Bodil': '555-666-789'
+    }
+    print(f"Antal i listan: {len(people)}")
+    vem = input("Vem vill du ringa?")
+    if vem not in people:
+        print("Sorry hörru, vet ej vem detta är. Har endast VIP i min katalog")
+    else:
+        number = people[vem]
+        print(f"Numret till {vem} är {number}")
 
 
-
+if __name__ == '__main__':
+    main()
