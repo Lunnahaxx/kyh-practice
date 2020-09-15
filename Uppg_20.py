03a539f42d48bed0dab59169b97cd06b9ba5f87f
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-15 19:41:35 +0200

    Added task info.

---
+++ Uppg_20.py
@@ -0,0 +1,36 @@
+"2.0.Uppg_20 "
+"Task info"
+"""
+Dags att använda sig av cheese shop (också kallat PyPI)!
+Setup -
+0. Fixa en .venv mapp via File->Settings->Project interpreter -> (kugghjul) -> Add
+(Obs! Var noga med att sökvägen till venv-mappen hamnar under eran kyh-practice så att den ligger i erat projekt och inte någon annanstans på hårddisken!)
+1. Se till så ni har ett .venv i era repo, markera detta directory "excluded".
+2. Skapa en fil "requirements.txt" i era kyh-practice repos, som innehåller bara raden "pygame"
+3. Kopiera "snake.py" ifrån Olofs kyh-practice repository (sök på kyh-practice på github.com).
+4. När PyCharm gnäller om att pygame saknas, klicka installera!
+
+Uppgifter
+20.1 Spela spelet lite och lägg märke till att ormen rör sig lite för fort för att det ska vara lätt att spela. Ändra ormens hastighet i koden!
+20.2 Det är roligare om ormen växer fortfare ... Hitta rätt ställe i koden för och väx ormen dubbelt så fort.
+20.3 Vilken av uppgift 1 och 2 gick fortare för er? Diskutera varför ni tror att den ena gick fortare än den andra.
+"""
+"-------------------------"
+"Koden 2.0.Uppg_20 "
+"Lösningen"
+
+"-------------------------"
+"-------------------------"
+"Förklaring till Lösningen på Koden"
+
+"-------------------------"
+"-------------------------"
+
+"""
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+"""
