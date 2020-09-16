74fe505345f6f0589522f3beabd06bdc936b17e1
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-16 10:04:11 +0200

    Kod under genomgång

--- Uppg_25.py
+++ Uppg_25.py
@@ -6,20 +6,20 @@ Fortsättning på uppgift 24 - mer om JSON och requests (webapi-anrop).
 
 Nu ska vi bearbeta datan något med hjälp av Python och f-string tricken jag visade er!
 
-25.1 Skriv ett Pythonprogram som använder requests mot URLen ni fick i uppgift 24, 
-och skriver ut kurstillfällen som sker under oktober 2020, på denna form:
+25.1 Skriv ett Pythonprogram som använder requests mot URLen ni fick i uppgift 24, och skriver ut kurstillfällen som sker under oktober 2020, på denna form:
 
-Kursnamn:      Professional Scrum Product Owner (EN), 15-16 september, Stockholm
-Startdatum:    2020-10-15
-Slutdatum:     2020-10-17
+   Kursnamn: Professional Scrum Product Owner (EN), 15-16 september, Stockholm
+ Startdatum: 2020-10-15
+   Slutdatum: 2020-10-17
 
-Kursnamn:      Professional Scrum Master, 15-16 september, Stockholm
-Startdatum:    2020-10-15
-Slutdatum:     2020-10-16
+   Kursnamn: Professional Scrum Master, 15-16 september, Stockholm
+ Startdatum: 2020-10-15
+   Slutdatum: 2020-10-16
 
-Osv.
+Den första kolumnen, fram till och med ":" är 12 tecken bred, och högerställd. 
+Den andra kolumen är 100 tecken bred, och vänsterställd.
 
-25.2 Istället för att hårdkoda oktober 2020 just, låt användaren mata in ett år och en månad,
+25.2 Istället för att hårdkoda oktober 2020 just, låt användaren mata in ett år och en månad, 
 som ni sedan använder för att filtrera ut kurserna som skrivs ut! T.ex.
 
    Year: 2020
@@ -28,14 +28,61 @@ som ni sedan använder för att filtrera ut kurserna som skrivs ut! T.ex.
 
 Tips: det går bra att använda -01 och -31 för att inkludera hela månaden, 
 även om vissa månader inte har så många dagar!
-
-
-
-
+"-------------------------"
 """
+"Koden 2.0.Uppg_25 "
+"Lösningen 16 Sept"
+""""""
+import requests
+import json
+import datetime
+from pprint import pprint
+
+r = requests.get('https://proagile.se/api/publicEvents')
+
+indata = json.loads(r.text)
+
+#hintar
+#today datytime type som, year.month.day  som int
+#jämnför vår datum tid i strängar < > går att skriva
+
+for i in indata:
+    if '2020' or '10' or '01' in i:
+        print("startDatum")
+        #print(i["2020-10-15"])
+
+#2020 10 == date
+
+
+#for i in indata:
+    #if i in (datetime.month)
+
+#datetime.month datetime.datetime.10()
+
+#in range
+#datetime.month
+#pprint(indata)
+
+#d = datetime.fromtimestamp(time.time())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 "-------------------------"
 "Koden 2.0.Uppg_25 "
-"Lösningen"
+"Lösningen 15 Sept"
 """
 Kursnamn:      Professional Scrum Product Owner (EN), 15-16 september, Stockholm
 Startdatum:    2020-09-15
@@ -46,7 +93,7 @@ Startdatum:    2020-09-15
 Slutdatum:     2020-09-16
 """
 #dagens_datum
-
+"""
 k1 = "Kursnamn:"
 s1 = "Startdatum:"
 a1 = "Slutdatum:"
@@ -83,6 +130,6 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main()"""
 
 
