6d7980145d4e7308cef8981d164f2a24b89022f5
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-17 11:37:49 +0200

    Added def m_to_mm Added def cm_to_m

---
+++ conversions.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+def m_to_mm():
+    value = int(input("Ange hur många meter du vill konvertera till millimeter: "))
+    m = value * 1000
+    print(m)
+
+
+m_to_mm()
+
+
+def cm_to_m():
+    value = int(input("Ange hur cm meter du vill konvertera till meter: "))
+    m = value * 0.01
+    print(m)
+
+cm_to_m()
