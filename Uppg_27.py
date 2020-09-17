0170f85c36ef6d01d805c2b3d8c7a99dc688871b
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-17 10:12:25 +0200

    Task 1.

--- Uppg_27.py
+++ Uppg_27.py
@@ -55,18 +55,40 @@ contents = [
     }
 ]
 
-27.2 Som ni kanske sett är det en lista på ingredienser i någon produkt - här är förlagan som ni kan titta på i en browser:
+27.2 Som ni kanske sett är det en lista på ingredienser i någon produkt - 
+här är förlagan som ni kan titta på i en browser:
 
       https://www.oboy.se/~/media/oboy/se/images/products/original-mobile.png
 
-Det är alltså data från kolumn 1 och 2. Bygg ett Pythonscript som skriver ut en liknande "tabell" på konsollen!
+Det är alltså data från kolumn 1 och 2. Bygg ett Pythonscript som skriver ut 
+en liknande "tabell" på konsollen!
 
 Tips: Tabellens första kolumn ska vara 25 tecken bred, och andra 12.
-Tips 2: Notera att det finns ett attribut "rightalign" i datan, som beskriver om "what" ska justeras till vänster eller höger!
+Tips 2: Notera att det finns ett attribut "rightalign" i datan, som 
+beskriver om "what" ska justeras till 
+vänster eller höger!
 """
 "-------------------------"
 "Koden 2.0.Uppg_ "
 "Lösningen"
+from pathlib import Path
+import json
+from pprint import pprint
+
+
+content = Path("data_U27.json").read_text(encoding='utf8')
+data = json.loads(content)
+
+for key in data:
+    #if "what" in key:
+    print(key["what"])
+
+#pprint(data)
+
+
+
+
+
 
 "-------------------------"
 "-------------------------"
@@ -74,11 +96,11 @@ Tips 2: Notera att det finns ett attribut "rightalign" i datan, som beskriver om
 
 "-------------------------"
 "-------------------------"
-
-
+"""
 def main():
     pass
 
 
 if __name__ == '__main__':
     main()
+"""
