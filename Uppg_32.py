941bef26070bb450848bfa07aa1864890b1f6310
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-22 09:48:20 +0200

    32.1

---
+++ Uppg_32.py
@@ -0,0 +1,30 @@
+"Uppg_Skriv ett Pythonprogram som tar in en sträng från användaren, och skriver ut följande:"
+
+
+"Task info"
+"""
+32.1 Längden på strängen
+32.2 Om strängen är ett "palindrom" eller ej. Exempel på palindrom: rattar, Ebbe. 
+Observera att programmet ska klara att användaren blandar stora och små bokstäver!
+32.3 Utöka programmet så att även uttryck som "Aja Baja" anses vara palindrom, d.v.s 
+filtrera bort alla mellanslag ifrån inputsträngen!
+
+Tips: använd det vi lärde oss igår kring att vända en sträng och/eller lista baklänges, 
+och join-funktionen. Även list comprehension kan komma till användning. "
+"""
+"-------------------------"
+"Koden Uppg_ "
+"Lösningen"
+strang = input("Skriv en sträng så får du längden: .")
+strang_lenght = len(strang)
+
+print(strang_lenght)
+
+
+
+"-------------------------"
+"-------------------------"
+"Förklaring till Lösningen på Koden"
+
+"-------------------------"
+"-------------------------"
