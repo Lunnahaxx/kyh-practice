786477a07de9252fbd803b71fac6535d0c8ca11f
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-23 11:41:33 +0200

    35.2 Done.

---
+++ Uppg_35.py
@@ -0,0 +1,58 @@
+"Uppg_35 "
+"Task info"
+"""
+En övning på POST-requests.
+
+Spara bifogad fil i en fil quizzservice.py först av allt!
+
+35.1 Undersök filen och försök förstå vad den gör. Diskutera i gruppen.
+
+35.2 I quizzservice.py syns ??? två gånger. Googla efter hur man POSTar JSON
+     med hjälp av requests och fyll i på båda ställena!
+
+35.3 Bygg ett pythonscript som skriver ut hur många frågor som finns i quizz-db.
+
+35.4 Utöka scriptet så att följande meny likt syns:
+
+   Det finns 3 frågor i quizz-db. Gör ett val:
+   1. Lägg till fråga
+   2. Avsluta programmet
+
+Val 1 ger användaren följande frågor:
+
+   Skriv en fråga: bla bla bla
+   Vad är rätt svar: bla bla bla
+   Ange några felaktiga svar (separera dem med kommateckan): bla,bla,bla
+   Tack för ditt bidrag!
+
+.. och sedan kommer hen tillbaka till huvudmenyn.
+"""
+"-------------------------"
+"Koden 2.0.Uppg_ "
+"Lösningen"
+
+
+
+
+
+
+
+
+
+
+
+
+"-------------------------"
+"-------------------------"
+"Förklaring till Lösningen på Koden"
+
+"-------------------------"
+"-------------------------"
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
