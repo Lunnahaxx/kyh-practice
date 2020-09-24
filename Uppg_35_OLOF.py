56cd3c852877e7680846b2da92cb2834d20d72a1
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-24 09:18:54 +0200

    35.3

--- Uppg_35_OLOF.py
+++ Uppg_35_OLOF.py
@@ -1,6 +1,6 @@
 import quizzservice
 
-if __name__ == 'main':
+if __name__ == '__main__':
     api = quizzservice.QuizzWebServiceAPI()
-    print(f"Antal frågor: {api.get_all_questions()}")
+    print(f"Antal frågor: {len(api.get_all_questions())}")
     #qs = quizzservice
