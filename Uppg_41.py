5fb21d1444f36e4a0bb07fdf8dcf433b0e0e02c4
Lunnehaxx <stairwaytobalken@gmail.com>
2020-09-29 12:19:11 +0200

    Added code.

---
+++ Uppg_41.py
@@ -0,0 +1,39 @@
+"2.0.Uppg_ "
+"Task info"
+"""
+Klona mitt kyh-practice repo (eller uppdatera om ni redan har det på datorn): 
+https://github.com/objarni/kyh-practice
+I det hittar ni en fil "scope1.py".
+
+1. Läs igenom koden UTAN ATT KÖRA DEN och diskutera vad som kommer skrivas ut tillsammans. Tycker alla samma sak?
+2. Kör nu koden, och undersök output. Blev det enligt förväntan? Diskutera!
+"""
+"-------------------------"
+"Koden 2.0.Uppg_ "
+"Lösningen"
+def fn(a):
+    print(a)
+    a = 2
+    print(a)
+
+a = 1
+print(a)
+fn(1)
+print(a)
+
+
+
+"-------------------------"
+"-------------------------"
+"Förklaring till Lösningen på Koden"
+"Den skriver ut koden på rad 20 först, får sedan in i funktioner."
+"-------------------------"
+"-------------------------"
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
