d69b5a885a59f5be6a33ca2eb94378e5518b1926
Lunnehaxx <stairwaytobalken@gmail.com>
2020-11-29 20:41:16 +0100

    Added for test.

--- class_example.py
+++ class_example.py
@@ -6,10 +6,12 @@ class School:
     def __init__(self, name):
         self.name = name
 
+    def sho(self):
+        pass
 
 
 
-#skola = School("KYH")
+#skola = School("KYH")s
 #print(skola.name) #man gess skola ett class som är name. Med punkt . kan man hämta istället för med [].
 
 #print(skola) #printar kyh
